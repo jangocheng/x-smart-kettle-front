5d38947e96564e58850e6acd017b9f365e841530
yaukie <yaukie@163.com>
2021-04-20 17:32:59 +0800

    Update dev.groovy

--- jenkins/dev.groovy
+++ jenkins/dev.groovy
@@ -1,46 +1,51 @@
 def label = "mypod-${UUID.randomUUID().toString()}"
+def project_group = "idmp"
+def project_url = 'git.yaukie.com/ecbh/idmp/business/frontend/'
 def project_name = "idmp-manage-vue"
 def img_name = "idmp-manage-vue"
 def img_container = "idmp_dev"
-def repo_url = "xtl-docker-local.repo.inspur.com";
+def repo_url = "xtl-docker-local.repo.yaukie.com";
 def img_url = ""
-def img_version = 'latest' //'${img_version}'
+def img_version = "latest"
+def repoUser = 'Xtl'
+def repoPassword = '123456a?'
+//ssh登录远程服务器 （服务器基础环境 docker、Git、node ）
+def remote = [:]
+  remote.name = 'yaukie'
+  remote.host = 'yaukie'
+  remote.user = 'root'
+  remote.password = '10Passw0rd'
+  remote.allowAnyHosts = true
 podTemplate(cloud: 'kubernetes',namespace: 'Xtldevops',label: label,serviceAccount:'bald-quetzal-ibm-jenkins',
     containers: [
-      containerTemplate(name: 'git', image: 'xtl-docker-local.repo.inspur.com/jenkins/gitlab-runner:11', ttyEnabled: true, command: 'cat', args: ''),
-      containerTemplate(name: 'yarn', image: 'xtl-docker-local.repo.inspur.com/jenkins/yarn:1.22.4', ttyEnabled: true, command: 'cat', args: ''),
-      containerTemplate(name: 'jnlp', image: "xtl-docker-local.repo.inspur.com/jenkins/jnlpwithssh:0.2", ttyEnabled: true, command: '', args: '${computer.jnlpmac} ${computer.name}'),
-      containerTemplate(name: 'docker', image: 'xtl-docker-local.repo.inspur.com/jenkins/docker-in-docker:18', ttyEnabled: true, command: 'cat', args: ''),
-      containerTemplate(name: 'kubectl', image: 'xtl-docker-local.repo.inspur.com/Xtl/kubectl3435:v2', ttyEnabled: true, command: 'cat', args: '')
-    ],imagePullSecrets: ['artifactory-Xtl'],volumes: [
+         containerTemplate(name: 'kubectl', image: 'xtl-docker-local.repo.yaukie.com/Xtl/kubectl3435:v2', ttyEnabled: true, command: 'cat', args: '')
+         ],imagePullSecrets: ['artifactory-Xtl'],volumes: [
         hostPathVolume ( hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')
     ])
     {
         node(label) {
-            stage('git-clone') {
-                container('git'){
-                    sh 'git clone http://e-commerce:e-commerce%3Fa@git.inspur.com/ecbh/idmp/business/frontend/idmp-manage-vue.git && git -C ' + project_name + ' show HEAD';
-                }
-            }
-            stage('yarn-build') {
-                container('yarn'){
-                    sh 'cd ' + project_name + ' && yarn  && yarn build';
-                }
-            }
-            stage('docker-build') {
-                container('docker'){
-                    sh 'docker login -u Xtl -p 123456a? xtl-docker-local.repo.inspur.com';
-                    sh 'cp -r ' + project_name + '/dist ' + project_name + '/docker/dev/dist';
-                    img_url = repo_url + '/'+img_container+'/' + img_name + ':' + img_version
-                    sh 'cd ' + project_name + '/docker/dev && docker build -t ' + img_url + ' .'
-                    sh 'docker push ' + img_url
-                }
+            stage('ssh-deploy') {
+                //删除已有目录 注意不同工程 project_name 不能重复
+                sshCommand remote: remote, command: 'cd /opt/buildSpace && if [ ! -d "' + project_group + '" ];then mkdir ' + project_group + '; fi && cd ' + project_group + ' && rm -rf '+project_name;
+                // git 下载工程
+                sshCommand remote: remote, command: 'cd /opt/buildSpace/' + project_group + ' && git clone http://e-commerce:e-commerce%3Fa@' + project_url + project_name +  '.git';
+                // node安装 并 编译
+                sshCommand remote: remote, command: 'cd /opt/buildSpace/' + project_group + '/' + project_name + '  &&  rm -Rf .git  && yarn install && yarn build ';
+                // 复制www 到 prod下
+                sshCommand remote: remote, command:  'cd /opt/buildSpace/' + project_group + '/' + project_name +'/ && cp -r  dist  ./docker/dev/';
+                //镜像名称
+                img_url = repo_url + '/'+ img_container +'/' + img_name + ':' + img_version;
+                //制作镜像
+                sshCommand remote: remote, command: 'cd  /opt/buildSpace/' + project_group + '/' + project_name +'/docker/dev   && docker build -t ' + img_url + ' .';
+                // 上传镜像
+                sshCommand remote: remote, command: 'docker login -u='+repoUser+' -p='+ repoPassword + ' ' + repo_url +' && docker push ' + img_url;
+                //删除已有目录
+                sshCommand remote: remote, command: 'cd /opt/buildSpace/' + project_group + ' &&  rm -rf '+project_name;
             }
             stage('kubectl-deploy') {
                 container('kubectl') {
                     sh 'kubectl rollout restart  deployment/idmp-manage-vue -n 1011034113';
                 }
             }
-
         }
     }
